547996e9a0190ad8d213b38d683b0c593740a797
Mayank Kheterpal <77799836+MayankKheterpal@users.noreply.github.com>
2021-10-13 08:44:31 -0400

    updated

--- patient-crud-app/src/components/PatientForm.tsx
+++ patient-crud-app/src/components/PatientForm.tsx
@@ -1,10 +1,4 @@
-import React, {
-  ChangeEvent,
-  EventHandler,
-  FormEvent,
-  useCallback,
-  useState,
-} from "react";
+import React, { ChangeEvent, useCallback, useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { v4 as uuidv4 } from "uuid";
 import { PatientModel } from "./Interface";
